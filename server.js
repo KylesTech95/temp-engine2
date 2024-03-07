6d79504844ff126e506646f250ae9f6354e5682c
kylestech95 <kylestech95@outlook.com>
2024-03-07 06:59:03 -0500

    pause: [200~Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 'use strict';
+const { ObjectID } = require('mongodb');
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session')
+const passport = require('passport')
 
 const app = express();
+
+// express session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave:true,
+  saveUninitialized:true,
+  cookie:{secure:false}
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.set('view engine','pug')
 app.set('views','./views/pug')
 fccTesting(app); //For FCC testing purposes
@@ -12,9 +28,23 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
 app.route('/').get((req, res) => {
-res.render('index')
+res.render('index',{title: 'Hello', message: 'Please log in'})   
 });
+
+
+// serial & deserialize users
+passport.serializeUser((user,done)=>{
+done(null,user._id);
+})
+passport.deserializeUser((id,done)=>{
+done(null,null)
+// myDatabase.findOne({_id: new ObjectID(id)},(err,done)=>{
+// done(null,null)
+// })
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
