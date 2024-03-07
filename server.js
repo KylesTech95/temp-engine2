e2b4536f33b8f028c03462f8797c38bab8b5b5be
kylestech95 <kylestech95@outlook.com>
2024-03-07 10:58:02 -0500

    feat: user logging out

--- server.js
+++ server.js
@@ -22,13 +22,17 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// middleware
 app.set('view engine','pug')
 app.set('views','./views/pug')
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// middleware function to ensure users are authenticated
+function ensureAuthenticated(req,res,next){
+  return req.isAuthenticated() ? next() : res.redirect('/')
+}
 
 myDB(async client =>{
   const myDataBase = await client.db('database').collection('users');
@@ -45,14 +49,21 @@ myDB(async client =>{
       res.redirect('/profile');
     })
     // GET login
-    app.route('/profile').get(function(req,res){
-      res.render('profile')
+    app.route('/profile').get(ensureAuthenticated,function(req,res){
+      res.render('profile',{username:req.user.username})
     })
+    
 
-
-
-
-
+    app.route('/logout').get((req,res)=>{
+      req.logout();
+      res.redirect('/')
+    })
+    // 404 Not Found
+    app.use((req,res,next)=>{
+      res.status(404)
+      .type('text')
+      .send('Not Found')
+    })
 
     // use LocalStrategy
     passport.use(new LocalStrategy((username,password,done)=>{
@@ -80,7 +91,7 @@ myDB(async client =>{
     done(null,user._id);
     })
     passport.deserializeUser((id,done)=>{
-    myDatabase.findOne({_id: new ObjectID(id)},(doc,done)=>{
+    myDataBase.findOne({_id: new ObjectID(id)},(doc,done)=>{
     done(null,doc)
     })
     })
