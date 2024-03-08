f16bfb88fd554820657163d7bbc46d32df788460
kylestech95 <kylestech95@outlook.com>
2024-03-08 07:22:53 -0500

    emit chat messages across all clients

--- public/client.js
+++ public/client.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
   /*global io*/
   let socket = io();
-  socket.on('user count',function(data){
+  socket.on('user',function(data){
     console.log(data)
+  })
+  // announce new users
+  socket.on('user', data => {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message =
+      data.username +
+      (data.connected ? ' has joined the chat.' : ' has left the chat.');
+    $('#messages').append($('<li>').html('<b>' + message + '</b>'));
+  });
+  // chat message
+  socket.on('chat message',message=>{
+    console.log('socket.on 1')
+    $('#messages').append($('<li>').text(`${message.username}: ${message.message}`));
   })
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
-    
+    socket.emit('chat message',messageToSend)
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
--- server.js
+++ server.js
@@ -11,13 +11,20 @@ const auth = require('./auth.js');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http)
-
+const passportSocketIo = require('passport.socketio')
+const MongoStore = require('connect-mongo')(session)
+const cookieParser = require('cookie-parser');
+const { SocketAddress } = require('net');
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({url:URI})
 // express session
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave:true,
   saveUninitialized:true,
-  cookie:{secure:false}
+  store:store,
+  cookie:{secure:false},
+  key:'express.sid'
 }))
 
 // passport middleware
@@ -34,20 +41,37 @@ app.use(express.urlencoded({ extended: true }));
 
 myDB(async client =>{
  const myDataBase = await client.db('database').collection('users');
-
+  
   routes(app,myDataBase)
   auth(app,myDataBase)
 
-  let currentUsers = 0;
+  // Auth with socket.io
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser:cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail:onAuthorizeFail
+    })
+  )
 
+  // Sockets! io
+  let currentUsers = 0;
   io.on('connection', socket=>{
     currentUsers++
-    io.emit('user count',currentUsers);
+    io.emit('user',{username:socket.request.user.username,currentUsers,connected:true});
+    
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { username: socket.request.user.username, message });
+    });
     console.log('A user has connected')
     socket.on('disconnect',()=>{
-      currentUsers++
-      io.emit('user count',currentUsers)
+      --currentUsers
+      io.emit('user',{username:currentUsers,connected:false});
       console.log('User disconnected')
+      console.log('user ' + socket.request.user.username + ' connected');
     })
   })
   
@@ -57,6 +81,18 @@ myDB(async client =>{
     res.render('index',{title:e,message:'Unable to connect to database'})
   })
 })
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 // app.listen
 const PORT = process.env.PORT || 3000;
